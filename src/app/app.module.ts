6f0f26242c7a98d4d9b41ea32adfedf4b5c837c1
vishnu1646 <vishnulance98@gmail.com>
2022-12-07 02:06:28 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddproductComponent
+  }
+  ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
